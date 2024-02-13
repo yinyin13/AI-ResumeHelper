238463c3a5e830f07a5f53aa850cf9fd309bb5c4
yinyin13 <yinyin13@uw.edu>
2024-02-13 15:45:26 -0800

    init

---
+++ app.py
@@ -0,0 +1,61 @@
+from tempfile import NamedTemporaryFile
+import os
+
+import streamlit as st
+from llama_index.core import VectorStoreIndex
+from llama_index.llms.openai import OpenAI
+from llama_index.readers.file import PDFReader
+
+st.set_page_config(
+    page_title="Chat with the PDF",
+    page_icon="🦙",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
+if "messages" not in st.session_state.keys():  # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about your document!"}
+    ]
+
+uploaded_file = st.file_uploader("Upload a file")
+if uploaded_file:
+    bytes_data = uploaded_file.read()
+    with NamedTemporaryFile(delete=False) as tmp:  # open a named temporary file
+        tmp.write(bytes_data)  # write data from the uploaded file into it
+        with st.spinner(
+            text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."
+        ):
+            reader = PDFReader()
+            docs = reader.load_data(tmp.name)
+            llm = OpenAI(
+                model="gpt-3.5-turbo",
+                temperature=0.0,
+                system_prompt="You are an expert on the content of the document, provide detailed answers to the questions. Use the document to support your answers.",
+            )
+            index = VectorStoreIndex.from_documents(docs)
+    os.remove(tmp.name)  # remove temp file
+
+    if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(
+            chat_mode="condense_question", verbose=False, llm=llm
+        )
+
+if prompt := st.chat_input(
+    "Your question"
+):  # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:  # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.stream_chat(prompt)
+            st.write_stream(response.response_gen)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)  # Add response to message history
